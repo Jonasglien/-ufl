57117a8acc359a3ec22fe68a9a026cef2a3e12e2
Jonas <jonasglien2@gmail.com>
2020-04-03 14:21:01 +0200

    functioning sliders

--- js/sliders.js
+++ js/sliders.js
@@ -8,83 +8,83 @@ slider1.oninput = function() {
 }
 
 //Slider 2
-var slider1 = document.getElementById("sensorslider1");
-var output1 = document.getElementById("sensorvalue1");
-output1.innerHTML = slider1.value;
+var slider2 = document.getElementById("sensorslider2");
+var output2 = document.getElementById("sensorvalue2");
+output2.innerHTML = slider2.value;
 
-slider1.oninput = function() {
-  output1.innerHTML = this.value;
+slider2.oninput = function() {
+  output2.innerHTML = this.value;
 }
 
 //Slider 3
-var slider1 = document.getElementById("sensorslider1");
-var output1 = document.getElementById("sensorvalue1");
-output1.innerHTML = slider1.value;
+var slider3 = document.getElementById("sensorslider3");
+var output3 = document.getElementById("sensorvalue3");
+output3.innerHTML = slider3.value;
 
-slider1.oninput = function() {
-  output1.innerHTML = this.value;
+slider3.oninput = function() {
+  output3.innerHTML = this.value;
 }
 
 //Slider 4
-var slider1 = document.getElementById("sensorslider1");
-var output1 = document.getElementById("sensorvalue1");
-output1.innerHTML = slider1.value;
+var slider4 = document.getElementById("sensorslider4");
+var output4 = document.getElementById("sensorvalue4");
+output4.innerHTML = slider4.value;
 
-slider1.oninput = function() {
-  output1.innerHTML = this.value;
+slider4.oninput = function() {
+  output4.innerHTML = this.value;
 }
 
 //Slider 5
-var slider1 = document.getElementById("sensorslider1");
-var output1 = document.getElementById("sensorvalue1");
-output1.innerHTML = slider1.value;
+var slider5 = document.getElementById("sensorslider5");
+var output5 = document.getElementById("sensorvalue5");
+output5.innerHTML = slider5.value;
 
-slider1.oninput = function() {
-  output1.innerHTML = this.value;
+slider5.oninput = function() {
+  output5.innerHTML = this.value;
 }
 
 //Slider 6
-var slider1 = document.getElementById("sensorslider1");
-var output1 = document.getElementById("sensorvalue1");
-output1.innerHTML = slider1.value;
+var slider6 = document.getElementById("sensorslider6");
+var output6 = document.getElementById("sensorvalue6");
+output6.innerHTML = slider6.value;
 
-slider1.oninput = function() {
-  output1.innerHTML = this.value;
+slider6.oninput = function() {
+  output6.innerHTML = this.value;
 }
 
 //Slider 7
-var slider1 = document.getElementById("sensorslider1");
-var output1 = document.getElementById("sensorvalue1");
-output1.innerHTML = slider1.value;
+var slider7 = document.getElementById("sensorslider7");
+var output7 = document.getElementById("sensorvalue7");
+output7.innerHTML = slider7.value;
 
-slider1.oninput = function() {
-  output1.innerHTML = this.value;
+slider7.oninput = function() {
+  output7.innerHTML = this.value;
 }
 
 //Slider 8
-var slider1 = document.getElementById("sensorslider1");
-var output1 = document.getElementById("sensorvalue1");
-output1.innerHTML = slider1.value;
+var slider8 = document.getElementById("sensorslider8");
+var output8 = document.getElementById("sensorvalue8");
+output8.innerHTML = slider8.value;
 
-slider1.oninput = function() {
-  output1.innerHTML = this.value;
+slider8.oninput = function() {
+  output8.innerHTML = this.value;
 }
 
 //Slider 9
-var slider1 = document.getElementById("sensorslider1");
-var output1 = document.getElementById("sensorvalue1");
-output1.innerHTML = slider1.value;
+var slider9 = document.getElementById("sensorslider9");
+var output9 = document.getElementById("sensorvalue9");
+output9.innerHTML = slider9.value;
 
-slider1.oninput = function() {
-  output1.innerHTML = this.value;
+slider9.oninput = function() {
+  output9.innerHTML = this.value;
 }
 
 //Slider 10
-var slider1 = document.getElementById("sensorslider1");
-var output1 = document.getElementById("sensorvalue1");
-output1.innerHTML = slider1.value;
+var slider10 = document.getElementById("sensorslider10");
+var output10 = document.getElementById("sensorvalue10");
+output10.innerHTML = slider10.value;
 
-slider1.oninput = function() {
-  output1.innerHTML = this.value;
+slider10.oninput = function() {
+  output10.innerHTML = this.value;
 }
 
